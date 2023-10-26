2c4ff600bb9e1ec9587ddc4bcb7860f9beeb1afd
athilalexandre <athilalexandre@gmail.com>
2023-10-26 15:24:59 -0300

    feat: Add views

--- routes/admin.js
+++ routes/admin.js
@@ -2,12 +2,14 @@ const express = require("express");
 
 const router = express.Router();
 
+// /admin/add=porduct => GET
 router.get("/add-product", (req, res, next) => {
   res.send(
     '<form action="/admin/add-product" method="POST"><input type="text" name="title"><button type="submit">Add Product</button></form>'
   );
 });
 
+// /admin/add=porduct => POST
 router.post("/add-product", (req, res, next) => {
   console.log(req.body);
   res.redirect("/");
--- routes/shop.js
+++ routes/shop.js
@@ -2,9 +2,7 @@ const express = require("express");
 const router = express.Router();
 
 router.get("/", (req, res, next) => {
-  res.send(
-    "<h1> 🎃 Hello from Express!🎃  </h1>  <h2> by Athila Alexandre</h2>"
-  );
+  res.sendFile("./views/shop.html");
 });
 
 module.exports = router;
